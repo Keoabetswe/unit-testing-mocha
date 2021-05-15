3f1f152e47445a08859f86604ffe76e746c0b6cc
Keoabetswe <keo.nthite@iress.com>
2021-05-15 20:32:57 +0200

    pass farenheit array values

---
+++ problem.js
@@ -0,0 +1,8 @@
+ // farenheit to celcius
+ // [23, 140, 212, 41] => [-5, 60, 100, 5]
+
+const farenheit = [23, 140, 212, 41];
+
+function getCelcius(farenheit) {
+   return farenheit.map(value => (value - 32) * 5/9);
+}
